eba95ba736300699d269c7cdf3a2dd8580ad4b49
Fredrik Strupe <frestr@users.noreply.github.com>
2015-11-01 15:53:49 +0100

    Add unicode flag

--- piecetype.py
+++ piecetype.py
@@ -1,3 +1,6 @@
+
+UNICODE = True
+
 class Piece():
     def setAttributes(self, movementPattern, bounded, symbol):
         self.movementPattern = movementPattern
@@ -14,30 +17,30 @@ class Pawn(Piece):
     # (0, 1) means "stay at x-axis, move one up at y-axis"
     def __init__(self):
         movementPattern = [(0, 1)] 
-        self.setAttributes(movementPattern, True, '\u2659')
+        self.setAttributes(movementPattern, True, '\u2659' if UNICODE else 'P')
 
 class Knight(Piece):
     def __init__(self):
         movementPattern = [(-2, 1), (-1, 2), (1, 2), (2, 1), (2, -1), (1, -2), (-1, -2), (-2, -1)]
-        self.setAttributes(movementPattern, True,  '\u2658')
+        self.setAttributes(movementPattern, True,  '\u2658' if UNICODE else 'N')
 
 class Queen(Piece):
     def __init__(self):
         movementPattern = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
-        self.setAttributes(movementPattern, False,  '\u2655')
+        self.setAttributes(movementPattern, False,  '\u2655' if UNICODE else 'Q')
 
 class Bishop(Piece):
     def __init__(self):
         movementPattern = [(-1, 1), (1, 1), (1, -1), (-1, -1)]
-        self.setAttributes(movementPattern, False,  '\u2657')
+        self.setAttributes(movementPattern, False,  '\u2657' if UNICODE else 'B')
 
 class Rook(Piece):
     def __init__(self):
         movementPattern = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-        self.setAttributes(movementPattern, False,  '\u2656')
+        self.setAttributes(movementPattern, False,  '\u2656' if UNICODE else 'R')
 
 class King(Piece):
     def __init__(self):
         movementPattern = [(-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1), (-1, -1)]
-        self.setAttributes(movementPattern, True,  '\u2654')
+        self.setAttributes(movementPattern, True,  '\u2654' if UNICODE else 'K')
 
